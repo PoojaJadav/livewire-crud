c93047f498f7bc1036e8a8bbf93159542bb93d32
Pooja Jadav <56014892+PoojaJadav@users.noreply.github.com>
2022-03-29 14:50:11 +0530

    livewire crud service provider refactor

--- src/LivewireCrudServiceProvider.php
+++ src/LivewireCrudServiceProvider.php
@@ -7,8 +7,6 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\View\Compilers\BladeCompiler;
 use Poojajadav\LivewireCrud\Commands\MakeLivewireCRUDCommand;
 use Poojajadav\LivewireCrud\Commands\StubsCommand;
-use Poojajadav\LivewireCrud\View\Components\Search;
-use Poojajadav\LivewireCrud\View\Components\Sort;
 
 class LivewireCrudServiceProvider extends ServiceProvider
 {
@@ -29,15 +27,13 @@ class LivewireCrudServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'livewire-crud');
+        $this->configureComponents();
+
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
         }
-    }
-
-    public function provides()
-    {
-        return ['livewire-crud'];
     }
 
     /**
@@ -47,42 +43,11 @@ class LivewireCrudServiceProvider extends ServiceProvider
      */
     protected function bootForConsole()
     {
-        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'livewire-crud');
-
-//        $this->registerBladeComponents();
-
-        $this->configureComponents();
-
-        // Registering package components
-        /*$this->loadViewComponentsAs('wire', [
-            Search::class,
-            Sort::class
-        ]);*/
-
         // Registering package commands.
         $this->commands([
             MakeLivewireCRUDCommand::class,
             StubsCommand::class
         ]);
-    }
-
-    protected function registerBladeComponents(): void
-    {
-        $this->callAfterResolving(BladeCompiler::class, function (BladeCompiler $blade) {
-            $components = [
-                'search'    => [
-                    'class' => Search::class,
-                    'alias' => 'search',
-                ],
-                'sort' => [
-                    'class' => Sort::class,
-                    'alias' => 'sort',
-                ]
-            ];
-            foreach ($components as $component) {
-                $blade->component($component['class'], $component['alias']);
-            }
-        });
     }
 
     protected function configureComponents()
@@ -95,6 +60,6 @@ class LivewireCrudServiceProvider extends ServiceProvider
 
     protected function registerComponent(string $component)
     {
-        Blade::component('livewire-crud::components.' . $component, 'wire-' . $component);
+        Blade::component('livewire-crud::components.' . $component, 'wire-'.$component);
     }
 }
